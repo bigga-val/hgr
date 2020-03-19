4257c39bb4855df6f7773184e0ffcfc5c28fa1ed
Hgr Mwangeji <gabrielkatonge@gmail.com>
2020-03-19 14:52:50 +0200

    firebase ajout ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { config } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hgr';
+  constructor(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyD4j-1poAjO8dRw5ZiFecHb42SXVI6x8Fc",
+      authDomain: "mwangejidispensaire.firebaseapp.com",
+      databaseURL: "https://mwangejidispensaire.firebaseio.com",
+      projectId: "mwangejidispensaire",
+      storageBucket: "mwangejidispensaire.appspot.com",
+      messagingSenderId: "1010197745648",
+      appId: "1:1010197745648:web:69cceb6edaadf80eef5735",
+      measurementId: "G-0YNJ28TGW9"
+    };
+    firebase.initializeApp(config);
+  }
+
 }
